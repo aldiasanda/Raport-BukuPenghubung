13408e76d895a9e497a4408e0bf9cc0e60e05658
al <alifiandiasanda@gmail.com>
2016-11-19 14:36:31 +0700

    Menu Slide

--- app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl501112131/raportbukupenghubung_kelompok1/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl501112131/raportbukupenghubung_kelompok1/MainActivity.java
@@ -3,11 +3,77 @@ package id.sch.smktelkom_mlg.project.xiirpl501112131.raportbukupenghubung_kelomp
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
+public class MainActivity extends AppCompatActivity {
+    private DrawerLayout dr;
+    private Toolbar tl;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tl = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(tl);
+        ProsesNavigasi();
+
+    }
+
+    public void ProsesNavigasi() {
+        NavigationView navigationView = (NavigationView) findViewById(R.id.navigation_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+
+                int id = menuItem.getItemId();
+
+                switch (id) {
+                    case R.id.home:
+                        Toast.makeText(getApplicationContext(), "Home", Toast.LENGTH_SHORT).show();
+                        dr.closeDrawers();
+                        break;
+                    case R.id.settings:
+                        Toast.makeText(getApplicationContext(), "About", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.trash:
+                        Toast.makeText(getApplicationContext(), "Guide", Toast.LENGTH_SHORT).show();
+                        dr.closeDrawers();
+                        break;
+                    case R.id.logout:
+                        Toast.makeText(getApplicationContext(), "Logout", Toast.LENGTH_SHORT).show();
+                        dr.closeDrawers();
+                        break;
+                }
+                return true;
+            }
+        });
+        View header = navigationView.getHeaderView(0);
+        TextView tv_email = (TextView) header.findViewById(R.id.tv_email);
+        tv_email.setText("Kelompok 1");
+        dr = (DrawerLayout) findViewById(R.id.drawer);
+
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this, dr, tl, R.string.drawer_open, R.string.drawer_close) {
+
+            @Override
+            public void onDrawerClosed(View v) {
+                super.onDrawerClosed(v);
+            }
+
+            @Override
+            public void onDrawerOpened(View v) {
+                super.onDrawerOpened(v);
+            }
+        };
+        dr.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
     }
 }
