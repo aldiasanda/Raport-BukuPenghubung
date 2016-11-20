e66b5df233a18f9038501396434f20b33661fcb8
Cicilia <cicilia_putri_23rpl@student.smktelkom-mlg.sch.id>
2016-11-20 18:37:19 +0700

    Layout Login

---
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl501112131/raportbukupenghubung_kelompok1/LoginActivity.java
@@ -0,0 +1,13 @@
+package id.sch.smktelkom_mlg.project.xiirpl501112131.raportbukupenghubung_kelompok1;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
