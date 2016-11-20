6e93c48f1e9f3e4253177132d58ef1057b0a2cd8
putri31 <sangfiad@gmail.com>
2016-11-20 21:44:38 +0700

    Add files via upload

--- get.php
+++ get.php
@@ -1,10 +1,9 @@
 <?php
  
 $no_absen = $_GET['no_absen']; 
-
 require_once('connect.php'); 
 
-$sql = "SELECT * FROM tb_siswa,tb_mapel WHERE no_absen=$no_absen,nis=$nis,nama=$nama,nilai=$nilai"; 
+$sql = "SELECT * FROM tb_siswa,tb_mapel WHERE no_absen=$no_absen,nis=$nis,nama=$nama,pai=$pai,pkn=$pkn,bi=$bi,mat=$mat,si=$si,bing=$bing,sbk=$sbk,pkwu=$pkwu,penjas=$penjas,adser=$adser,rbj=$rbj,web=$web,grafik=$grafik,pbb=$pbb,adbasdat=$sdbasdat"; 
 
 $r = mysqli_query($con,$sql); 
 
@@ -12,7 +11,21 @@ $result = array(); $row = mysqli_fetch_array($r);
 array_push($result,array( "no_absen"=>$row['no_absen'],
 "nis"=>$row['nis'],
 "nama"=>$row['nama'],
-"jurusa"=>$row['jurusan']
+"pai"=>$row['pai']
+"pkn"=>$row['pkn']
+"bi"=>$row['bi']
+"mat"=>$row['mat']
+"bing"=>$row['bing']
+"sbk"=>$row['sbk']
+"pkwu"=>$row['pkwu']
+"penjas"=>$row['penjas']
+"adser"=>$row['adser']
+"rbj"=>$row['rbj']
+"web"=>$row['web']
+"grafik"=>$row['grafik']
+"pbb"=>$row['pbb']
+"adbasdat"=>$row['adbasdat']
+
 ));
   
 
