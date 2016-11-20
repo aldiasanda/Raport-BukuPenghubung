512b664791fb72b0b93b346dcdee414913bf3946
al <alifiandiasanda@gmail.com>
2016-11-20 08:40:46 +0700

    Menu Tab Add

--- app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl501112131/raportbukupenghubung_kelompok1/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl501112131/raportbukupenghubung_kelompok1/MainActivity.java
@@ -1,29 +1,115 @@
 package id.sch.smktelkom_mlg.project.xiirpl501112131.raportbukupenghubung_kelompok1;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements TabLayout.OnTabSelectedListener {
+    private DrawerLayout dr;
+    private Toolbar tl;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        tl = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(tl);
+        ProsesNavigasi();
 
+        //Inisiaiasi tablayout
+        tabLayout = (TabLayout) findViewById(R.id.tabLayout);
+
+        //Tambahkan Tabs menggunakan addTab method
+        tabLayout.addTab(tabLayout.newTab().setText("Nilai Akademik"));
+        tabLayout.addTab(tabLayout.newTab().setText("Nilai Sikap"));
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+
+        //Inisialisasi viewPager
+        viewPager = (ViewPager) findViewById(R.id.pager);
+
+        //Buat pager adapter
+        Pager adapter = new Pager(getSupportFragmentManager(), tabLayout.getTabCount());
+
+        //Tambahkan adapter ke pager
+        viewPager.setAdapter(adapter);
+
+        //Tambahkan onTabSelectedListener ke mode swipe views
+        tabLayout.setOnTabSelectedListener(this);
+    }
+
+    @Override
+    public void onTabSelected(TabLayout.Tab tab) {
 
     }
 
+    @Override
+    public void onTabUnselected(TabLayout.Tab tab) {
 
+    }
+
+    @Override
+    public void onTabReselected(TabLayout.Tab tab) {
+
+    }
+
+    public void ProsesNavigasi() {
+        NavigationView navigationView = (NavigationView) findViewById(R.id.navigation_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+
+                int id = menuItem.getItemId();
+
+                switch (id) {
+                    case R.id.home:
+                        Toast.makeText(getApplicationContext(), "Home", Toast.LENGTH_SHORT).show();
+                        dr.closeDrawers();
+                        break;
+                    case R.id.settings:
+                        Toast.makeText(getApplicationContext(), "About", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.trash:
+                        Toast.makeText(getApplicationContext(), "Guide", Toast.LENGTH_SHORT).show();
+                        dr.closeDrawers();
+                        break;
+                    case R.id.logout:
+                        Toast.makeText(getApplicationContext(), "Logout", Toast.LENGTH_SHORT).show();
+                        dr.closeDrawers();
+                        break;
+                }
+                return true;
+            }
+        });
+        View header = navigationView.getHeaderView(0);
+        TextView tv_email = (TextView) header.findViewById(R.id.tv_email);
+        tv_email.setText("Kelompok 1");
+        dr = (DrawerLayout) findViewById(R.id.drawer);
+
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this, dr, tl, R.string.drawer_open, R.string.drawer_close) {
+
+            @Override
+            public void onDrawerClosed(View v) {
+                super.onDrawerClosed(v);
+            }
+
+            @Override
+            public void onDrawerOpened(View v) {
+                super.onDrawerOpened(v);
+            }
+        };
+        dr.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+    }
 }
+
