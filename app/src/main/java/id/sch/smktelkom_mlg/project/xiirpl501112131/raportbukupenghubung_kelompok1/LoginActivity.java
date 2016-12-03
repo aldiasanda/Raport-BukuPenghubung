f5a259539578d46a3eb5d65cb0e81ff67959a892
al <alifiandiasanda@gmail.com>
2016-12-03 12:40:20 +0700

    Tambah Login

--- app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl501112131/raportbukupenghubung_kelompok1/LoginActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl501112131/raportbukupenghubung_kelompok1/LoginActivity.java
@@ -1,99 +1,86 @@
 package id.sch.smktelkom_mlg.project.xiirpl501112131.raportbukupenghubung_kelompok1;
 
+
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.android.volley.AuthFailureError;
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
-import java.util.HashMap;
-import java.util.Map;
 
-public class LoginActivity extends AppCompatActivity {
-    public static final String LOGIN_URL = "http://192.168.1.109/Android/login.php";
-    public static final String KEY_USERNAME = "username";
-    public static final String KEY_PASSWORD = "password";
-    EditText etUsername, etPassword;
-    Button btn;
-    private String username;
-    private String password;
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+    private EditText etEmail;
+    private EditText etPassword;
+    private Button btLogin;
 
+    private FirebaseAuth firebaseAuth;
+
+    private ProgressDialog pDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        etUsername = (EditText) findViewById(R.id.editTextUsername);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        if (firebaseAuth.getCurrentUser() != null) {
+            finish();
+            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+        }
+
+        etEmail = (EditText) findViewById(R.id.editTextEmail);
         etPassword = (EditText) findViewById(R.id.editTextPassword);
-        btn = (Button) findViewById(R.id.button);
-        btn.setOnClickListener(new View.OnClickListener() {
+        btLogin = (Button) findViewById(R.id.buttonLogin);
+
+        pDialog = new ProgressDialog(this);
+
+        btLogin.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == btLogin) {
+            userLogin();
+        }
+    }
+
+    private void userLogin() {
+        String email = etEmail.getText().toString().trim();
+        String password = etPassword.getText().toString().trim();
+
+        if (TextUtils.isEmpty(email)) {
+            Toast.makeText(this, "Mohon Masukkan Email", Toast.LENGTH_LONG).show();
+            return;
+        }
+        if (TextUtils.isEmpty(password)) {
+            Toast.makeText(this, "Mohon Masukkan Password", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        pDialog.setMessage("Login in...");
+        pDialog.show();
+
+        firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
             @Override
-            public void onClick(View view) {
-                Login();
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                pDialog.dismiss();
+                if (task.isSuccessful()) {
+                    finish();
+                    startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                } else {
+                    Toast.makeText(LoginActivity.this, task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                }
             }
         });
-    }
-
-    private void Login() {
-        final ProgressDialog pDilog = new ProgressDialog(LoginActivity.this);
-        pDilog.setIndeterminate(true);
-        pDilog.setMessage("Please Wait..");
-        pDilog.show();
-
-        username = etUsername.getText().toString().trim();
-        password = etPassword.getText().toString().trim();
-
-        StringRequest stringequest = new StringRequest(Request.Method.POST, LOGIN_URL,
-                new Response.Listener<String>() {
-
-                    @Override
-                    public void onResponse(String response) {
-                        if (response.trim().equals("success")) {
-                            pDilog.dismiss();
-                            LoginSuccess();
-                        } else if (response.trim().equals("failure")) {
-                            pDilog.dismiss();
-                            LoginFailed();
-                        }
-                    }
-                },
-                new Response.ErrorListener() {
-
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        pDilog.dismiss();
-                        Toast.makeText(LoginActivity.this, error.toString(), Toast.LENGTH_LONG).show();
-                    }
-                }) {
-            @Override
-            protected Map<String, String> getParams() throws AuthFailureError {
-                Map<String, String> map = new HashMap<String, String>();
-                map.put(KEY_USERNAME, username);
-                map.put(KEY_PASSWORD, password);
-                return map;
-            }
-        };
-
-        RequestQueue requestQueue = Volley.newRequestQueue(this);
-        requestQueue.add(stringequest);
-    }
-
-    private void LoginSuccess() {
-        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-        startActivity(intent);
-    }
-
-    private void LoginFailed() {
-        Toast.makeText(getBaseContext(), "Wrong Username Or Password", Toast.LENGTH_LONG).show();
     }
 }
--- app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl501112131/raportbukupenghubung_kelompok1/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl501112131/raportbukupenghubung_kelompok1/MainActivity.java
@@ -1,115 +1,52 @@
 package id.sch.smktelkom_mlg.project.xiirpl501112131.raportbukupenghubung_kelompok1;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.NavigationView;
-import android.support.design.widget.TabLayout;
-import android.support.v4.view.ViewPager;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements TabLayout.OnTabSelectedListener {
-    private DrawerLayout dr;
-    private Toolbar tl;
-    private TabLayout tabLayout;
-    private ViewPager viewPager;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private TextView tvEmail;
+    private Button bLogout;
+    private FirebaseAuth firebaseAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        tl = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(tl);
-        ProsesNavigasi();
+        firebaseAuth = FirebaseAuth.getInstance();
 
-        //Inisiaiasi tablayout
-        tabLayout = (TabLayout) findViewById(R.id.tabLayout);
+        if (firebaseAuth.getCurrentUser() == null) {
+            finish();
+            startActivity(new Intent(this, LoginActivity.class));
+        }
 
-        //Tambahkan Tabs menggunakan addTab method
-        tabLayout.addTab(tabLayout.newTab().setText("Nilai Akademik"));
-        tabLayout.addTab(tabLayout.newTab().setText("Nilai Sikap"));
-        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+        FirebaseUser user = firebaseAuth.getCurrentUser();
 
-        //Inisialisasi viewPager
-        viewPager = (ViewPager) findViewById(R.id.pager);
+        tvEmail = (TextView) findViewById(R.id.textViewUserEmail);
 
-        //Buat pager adapter
-        Pager adapter = new Pager(getSupportFragmentManager(), tabLayout.getTabCount());
+        if (user != null) {
+            tvEmail.setText("Selamat Datang " + user.getEmail());
+        }
 
-        //Tambahkan adapter ke pager
-        viewPager.setAdapter(adapter);
+        bLogout = (Button) findViewById(R.id.buttonLogut);
 
-        //Tambahkan onTabSelectedListener ke mode swipe views
-        tabLayout.setOnTabSelectedListener(this);
+        bLogout.setOnClickListener(this);
     }
 
     @Override
-    public void onTabSelected(TabLayout.Tab tab) {
-
-    }
-
-    @Override
-    public void onTabUnselected(TabLayout.Tab tab) {
-
-    }
-
-    @Override
-    public void onTabReselected(TabLayout.Tab tab) {
-
-    }
-
-    public void ProsesNavigasi() {
-        NavigationView navigationView = (NavigationView) findViewById(R.id.navigation_view);
-        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(MenuItem menuItem) {
-
-                int id = menuItem.getItemId();
-
-                switch (id) {
-                    case R.id.home:
-                        Toast.makeText(getApplicationContext(), "Home", Toast.LENGTH_SHORT).show();
-                        dr.closeDrawers();
-                        break;
-                    case R.id.settings:
-                        Toast.makeText(getApplicationContext(), "About", Toast.LENGTH_SHORT).show();
-                        break;
-                    case R.id.trash:
-                        Toast.makeText(getApplicationContext(), "Guide", Toast.LENGTH_SHORT).show();
-                        dr.closeDrawers();
-                        break;
-                    case R.id.logout:
-                        Toast.makeText(getApplicationContext(), "Logout", Toast.LENGTH_SHORT).show();
-                        dr.closeDrawers();
-                        break;
-                }
-                return true;
-            }
-        });
-        View header = navigationView.getHeaderView(0);
-        TextView tv_email = (TextView) header.findViewById(R.id.tv_email);
-        tv_email.setText("Kelompok 1");
-        dr = (DrawerLayout) findViewById(R.id.drawer);
-
-        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this, dr, tl, R.string.drawer_open, R.string.drawer_close) {
-
-            @Override
-            public void onDrawerClosed(View v) {
-                super.onDrawerClosed(v);
-            }
-
-            @Override
-            public void onDrawerOpened(View v) {
-                super.onDrawerOpened(v);
-            }
-        };
-        dr.addDrawerListener(actionBarDrawerToggle);
-        actionBarDrawerToggle.syncState();
+    public void onClick(View view) {
+        if (view == bLogout) {
+            firebaseAuth.signOut();
+            finish();
+            startActivity(new Intent(this, LoginActivity.class));
+        }
     }
 }
 
